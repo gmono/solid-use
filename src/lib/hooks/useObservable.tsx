b29ac92c90b398181e78dc52387686e05b10c1d4
gmono <gaozijian0411@foxmail.com>
2024-04-05 21:27:07 +0800

    添加useObservable

---
+++ src/lib/hooks/useObservable.tsx
@@ -0,0 +1,18 @@
+﻿import {Observable} from "rxjs"
+import { createSignal, onCleanup } from "solid-js"
+/**
+ * 使用一个可观察的值并返回一个signal
+ * @param obj 目标Observable
+ * @returns 
+ */
+export default function useObservable<T>(obj:Observable<T>){
+    const [value,setvalue]=createSignal<T|undefined>(undefined)
+    let sub=obj.subscribe(v=>{
+        setvalue(()=>v)
+    })
+    onCleanup(()=>{
+        sub.unsubscribe()
+    })
+    return [value,setvalue]
+
+}
